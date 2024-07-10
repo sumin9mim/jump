b4bb63a0649f9a80a69873e679d67dee1bbb7b5c
sumin9mim <sumin.forest9@gmail.com>
2024-07-10 14:52:50 +0900

    aa

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
-import { useForm } from 'react-hook-form';
+// import { useForm } from 'react-hook-form';
 import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className='flex flex-col min-h-screen'>
-      <Link href='/boardlist'>Board</Link>
+      <Link href='/boardwrite'>Board</Link>
     </main>
   );
 }
 
 const HookForm = () => {
-  const {} = useForm();
+  // const {} = useForm();
   return (
     <form>
       <input type='text' placeholder='ID' />
